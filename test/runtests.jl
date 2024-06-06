c2cbc8eac48db316b6c15efb367a8dcf096d6a7c
dhtan <dinghangtan@gmail.com>
2024-06-06 16:12:54 +0800

    [+] add `multy config` in `runtests.jl`

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,28 +2,26 @@ using HeatFlowTopOpt
 using Logging 
 using Gridap 
 
-N = 480
-prefix = "vtk_test/test_$N"
-pvd = createpvd(prefix)
-lg = ConsoleLogger()
+const lg = ConsoleLogger()
 
 vec_configs = [
     # pde parameter
     "β₁" => 0.,
-    "β₂" => 0.1,
-    "β₃" => 1,
+    "β₂" => 1.,
+    "β₃" => 1.,
     "δt" => 8e-3,
     "α⁻" => 417.5,
     "α₋" => 0.,
     "kf" => 0.1624,
     "ks" => 40.47,
     "Re" => 5988.,
-    "γ" => 1027.6,
+    "γ" => [1027.6, 1e5],
     "Ts" => 1.,
     "ud⋅n" => 0.,
     "Td" => 0.,
-    "g⋅n" => 10.,
+    "g⋅n" => 1e-3,
     "Ts" => 1.,
+
 
     # motion paramter
     "up" => 0.95,
@@ -36,25 +34,33 @@ vec_configs = [
     "ϵ_ratio" => 0.5,
     "ϵ" => 20., 
     "save_iter" => 1,
-    "vol" => 0.4,
-    "max_it" => 5,
-    "InitType" => "Line",
+    "vol" => 0.3,
+    "max_it" => 10,
+    "InitType" => "Rand",
     "is_correct" => true,
     "is_restart" => false,       # if not correction, then restart is off.
     "is_vol_constraint" => true, # if false, then set val to a scalar.
     "is_bdupdate" => false,
 
     # model parameter
-    "N" => N,  # 240 for Line initialization, 240 ÷ 2 ÷ 20
+    "N" => 10,  # 240 for Line initialization, 240 ÷ 2 ÷ 20
     "dim" => 2,
-    "L" => 1.
+    "L" => 1.,
 ];
 
-# debug
-HeatFlowTopOpt.singlerun(Dict(vec_configs), prefix, pvd, lg; debug= true)
-savepvd(pvd)
+base_config, appended_config_arr = HeatFlowTopOpt.parse_vec_configs(vec_configs)
+map(eachindex(appended_config_arr)) do i
+    config = merge(base_config, Dict(appended_config_arr[i]...))
+    N = config["N"]
+    prefix = "vtk_test/temp"
+    pvd = createpvd(prefix)
 
-# ## run
-# run_with_configs(vec_configs, "----") 
+    # debug
+    HeatFlowTopOpt.singlerun(config, prefix, pvd, lg, 1; debug= true)
+    savepvd(pvd)
+
+end
+
+
 
 
