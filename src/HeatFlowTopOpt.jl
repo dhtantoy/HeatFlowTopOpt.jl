0a3d46b5c621bf6fde6abcd541658fc1a6b00121
dhtan <dinghangtan@gmail.com>
2024-06-27 12:51:14 +0800

    add comment on # of threads of BLAS.

--- src/HeatFlowTopOpt.jl
+++ src/HeatFlowTopOpt.jl
@@ -29,6 +29,10 @@ using DataFrames
 using ValueHistories
 using VideoIO
 
+# # always use OPENBLAS_NUM_THREADS=1 if your application is multithreaded while
+# # using OpenBLAS. This is to avoid oversubscription of threads.
+# BLAS.set_num_threads(1)
+
 export run_with_configs
 export domain2mp4
 
