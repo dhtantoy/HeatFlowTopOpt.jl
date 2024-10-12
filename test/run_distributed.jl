8234cf2163a6364f91d4b2295057b9b618c8fbb3
dhtan <dinghangtan@gmail.com>
2024-10-12 14:05:17 +0800

    remove `pkglock`

--- test/run_distributed.jl
+++ test/run_distributed.jl
@@ -1,17 +1,14 @@
-push!(LOAD_PATH, "@pkglock"); 
-using PkgLock; 
-PkgLock.instantiate_precompile()
-
+ENV["JULIA_CPU_TARGET"] = "generic;icelake-server,clone_all;skylake-avx512,clone_all;skylake,clone_all"
+using HeatFlowTopOpt
 using Distributed
-
 # # run at 138
 addprocs(
         [
-            ("c95", 2),
-            ("c0130",2),
-            ("yhxiang@197", 2),
-            ("c0123", 2),
-            ("c0124", 2),
+            ("c95", 1),
+            ("c0130",1),
+            ("yhxiang@197", 1),
+            ("c0123", 1),
+            ("c0124", 1),
         ], 
         # 2,
         tunnel= true,
@@ -20,49 +17,42 @@ addprocs(
         exeflags="--project"
     )
 
-# # interrupt the process when exit
-atexit() do 
-    for p in workers()
-        rmprocs(p)
-    end
-end
 
 @everywhere using HeatFlowTopOpt
-@everywhere using MKL
 
 vec_configs = [
     # pde parameter
     "β₁" => 0.,
     "β₂" => 0.,
-    "β₃" => 1.,
+    "β₃" => 1,
     "δt" => 0.,
-    "α⁻" => [10, 1000., ],
+    "α⁻" => [100, 200],
     "kf" => 0.,
-    "ks" => 10.,
-    "Re" => [10, 100.],
+    "ks" => 10,
+    "Re" => [300, 400, 500],
     "Pr" => 6.7,
     "Ts" => 1.,
-    "Vdₓ" => [0.1, 0.01, 100, 1000],
+    "Vdₓ" => 0.,
     "Td" => 0.0,
-    "Pd" => 0.,
+    "Pd" => 1.,
     "Ts" => 1.,
 
     # motion paramter
     "up" => 0.95,
     "down" => 0.05,
-    "τ₀" => 1.5,
-    "motion_type" => "gf",
+    "τ₀" => 1e-4,
+    "motion_type" => "conv",
     "pdsz" => 3,
 
     # top opt parameter
-    "correct_rate" => 0.5,
+    "correct_rate" => 0.8,
     "ϵ_ratio" => 0.5,
-    "ϵ" => 20., 
-    "save_iter" => 200,
+    "ϵ" => 10., 
+    "save_iter" => 100,
     "save_start" => 0,
-    "vol" => 0.7,
-    "max_it" => 200,
-    "InitType" => ["Line", "Lines"],
+    "vol" => [0.6, 0.7],
+    "max_it" => 100,
+    "InitType" => ["Line", "Lines", "Rand"],
     "InitFile" => "",
     "InitKey" => "",
     "scheme" => SCHEME_NULL,
@@ -73,6 +63,6 @@ vec_configs = [
     "Nc" => 300,  # 240 for Line initialization, 240 ÷ 2 ÷ 20
     "ModelFile" => "lcr_model"
 ];
-comments = "newmodel"
+comments = "test newer config"
 
 run_with_configs(vec_configs, comments)
