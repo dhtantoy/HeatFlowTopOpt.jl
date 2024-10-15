7912b41e5c0fb20e0552a21018797547c050502d
dhtan <dinghangtan@gmail.com>
2024-10-15 13:09:47 +0800

    fix bug of `PermArray`

--- src/update.jl
+++ src/update.jl
@@ -1,9 +1,9 @@
 """
-    random_window!(A, kernel, vol, i)
-in-place generation of a random `0-1` `kernel` with seed `i` and volume `vol`,
+    random_window!(A::Array, kernel::Array, ratio::Real, seed)
+in-place generation of a random `0-1` `kernel` with seed and volume `vol`,
 and upsample and store it to the size of `A` as a window.
 """
-function random_window!(A, kernel, ratio, seed) 
+function random_window!(A::Array, kernel::Array, ratio::Real, seed) 
     rand!(Random.seed!(seed), kernel)
     T = eltype(A)
 
@@ -26,7 +26,7 @@ end
 in-place update of `A` with `B` and `w` as the weight. `(1-w)A + wB -> A`, i.e.
 `A + w(B - A) -> A`.
 """
-function post_interpolate!(A, B, w::Real)
+function post_interpolate!(A::Array, B::Array, w::Real)
     r = 1 - w
     @turbo for i = eachindex(A)
         A[i] = B[i] * w + A[i] * r
@@ -39,7 +39,7 @@ end
 in-place update of `A` with `B` and `W` as the weight. `(1-W)⊗A + W⊗B -> A`, i.e. 
 `A + W⊗(B - A) -> A`.
 """
-function post_interpolate!(A, B, W::AbstractArray{T}) where T
+function post_interpolate!(A::Array, B::Array, W::Array{T}) where T
     @turbo for i = eachindex(A)
         A[i] = B[i] + W[i] * (A[i] - B[i])
         # A[i] = B[i] * W[i] + A[i] * (one(T) - W[i])
@@ -51,7 +51,7 @@ end
     post_phi!(Φ, Gτχ, Φ_max, Φ_min, [down, up])
 in-place post processing of `Φ` with `Gτχ` and `down` and `up` as the threshold.
 """
-function post_phi!(Φ::AbstractArray{T1}, Gτχ::AbstractArray{T2}, Φ_max::T1, Φ_min::T1, down::T2= typemin(T2), up::T2= typemax(T2)) where {T1, T2}
+function post_phi!(Φ::Array{T1}, Gτχ::Array{T2}, Φ_max::T1, Φ_min::T1, down::T2= typemin(T2), up::T2= typemax(T2)) where {T1, T2}
     Φ_max += one(T1)
     Φ_min -= one(T1)
     @inbounds for i = eachindex(Φ)
@@ -68,7 +68,7 @@ end
     rand_post_phi!(Φ, perm, Φ_max, n, seed)
 in-place post porcessing, setting `n` random elements in `Φ` to `typemax(T)`.
 """
-function rand_post_phi!(Φ::AbstractArray{T}, perm::AbstractArray{<:Integer}, Φ_max::T, n, seed) where {T}
+function rand_post_phi!(Φ::Array{T}, perm::Array{<:Integer}, Φ_max::T, n, seed) where {T}
     randperm!(Random.seed!(seed), perm)
     Φ_max += one(T)
     @turbo for j = 1:n
@@ -83,7 +83,7 @@ end
     iterateχ!(χ, order, Φ, M)
 in-place iteration of `χ` with volume `M`, and the ascending order of selected `Φ` is stored in `order`.
 """
-function iterateχ!(χ, order::SzVector{Int}, Φ::AbstractArray{T}, M) where {T}
+function iterateχ!(χ::Array{T}, order::SzVector{Int}, Φ::AbstractArray{T}, M::Real) where {T}
     fill!(χ, zero(T))
     ix = order.data
 
@@ -109,7 +109,7 @@ end
     get_sorted_idx!(order_idx, cache_val, cache_idx, χ, weight)
 in-place computation of ascendingly ordered indices of selected `weight` (`χ` = 1).
 """
-function get_sorted_idx!(order_idx::AbstractVector, cache_val::SzVector, cache_idx::SzVector, χ, weight)
+function get_sorted_idx!(order_idx::SzVector, cache_val::SzVector, cache_idx::SzVector, χ, weight)
     resize!(cache_val, 0)
     resize!(cache_idx, 0)
     T = eltype(χ)
@@ -138,7 +138,7 @@ end
 in-place computation of probability (rescaled `Φ`) stored in `P`, and where Φ is smaller
 has larger probability.
 """
-function phi_to_prob!(P, Φ::AbstractArray{T}, Φ_max::T) where {T}
+function phi_to_prob!(P::Array, Φ::Array{T}, Φ_max::T) where {T}
     Φ_max += one(T)     # avoid zero probability
     d = 1. / (length(Φ) * Φ_max - sum(Φ))
     @turbo for i = eachindex(Φ)
@@ -153,7 +153,7 @@ end
 in-place computation of weight stored in `weight` with probability `P`, and 
 where P is larger has smaller `weight`.
 """
-function prob_to_weight!(weight, P, cache_idx::AbstractArray{T}) where {T <: Integer}
+function prob_to_weight!(weight::Array, P::Array, cache_idx::Array{T}) where {T <: Integer}
     a = one(T):length(P)
     wv = pweights(P)
     sample!(a, wv, cache_idx; replace= false)
@@ -171,7 +171,7 @@ end
 in-place correction of `χ` with rate `θ` and update `sort_idx_dec` and `sort_idx_inc`, 
 where `sort_idx_dec` (1->0) and `sort_idx_inc` (0->1) are ascendingly sorted indices.
 """
-function nonsym_correct!(χ, sort_idx_dec, sort_idx_inc, θ)
+function nonsym_correct!(χ::Array, sort_idx_dec::SzVector, sort_idx_inc::SzVector, θ::Real)
     na = length(sort_idx_dec)
     nb = length(sort_idx_inc)
     @assert 0 < θ < 1 "θ should be in (0, 1)."
